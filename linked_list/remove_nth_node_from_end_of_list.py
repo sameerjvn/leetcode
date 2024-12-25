af65b2e1e98e410fd64e28db4e079533e224a63e
Sameer Jathavedan <sameerjvn.tech@gmail.com>
2024-12-25 14:55:26 +0100

    linked list: #19 remove nth node from end of list

--- linked_list/remove_nth_node_from_end_of_list.py
+++ linked_list/remove_nth_node_from_end_of_list.py
@@ -12,6 +12,33 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # two pointers (single pass)
+        # time complexity: O(n)
+        # space complexity: O(1)
+
+        # start with a dummy node so that single element case can be handled
+        new_head = ListNode()
+        new_head.next = head
+
+        # shift right pointer till the distance from end is satisfied
+        left, right = new_head, new_head.next
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        # move both pointers till right pointer hits the end of linked list
+        while right:
+            left = left.next
+            right = right.next
+
+        # make left node skip its next node
+        left.next = left.next.next
+
+        return new_head.next
+
+
+
+    def _removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         # iteration (two pass)
         # time complexity: O(n)
         # space complexity: O(1)
