484167fa34a2d66206daa5a0db8990d389348977
Sameer Jathavedan <sameerjvn.tech@gmail.com>
2024-12-27 21:11:30 +0100

    graphs: #200 number of islands (dfs)

--- graphs/number_of_islands.py
+++ graphs/number_of_islands.py
@@ -48,3 +48,43 @@ class Solution:
                     num_islands += 1
 
         return num_islands
+
+    def _numIslands(self, grid: list[list[str]]) -> int:
+        # dfs
+        # time complexity: O(m * n)
+        # space complexity: O(m * n)
+
+        num_islands = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        def dfs(r, c):
+
+            # define base case, because dfs is recursive
+            if (
+                r < 0
+                or c < 0
+                or r >= ROWS
+                or c >= COLS
+                or grid[r][c] == "0"
+            ):
+                return
+
+            # modify current element so we don't cross it again
+            grid[r][c] = "0"
+
+            # do the actual depth first search
+            # since it's recursive, 
+            # the traversal will only change direction if
+            # a boundary condition is hit 
+            for dr, dc in directions:
+                new_row, new_col = r + dr, c + dc
+                dfs(new_row, new_col)
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == "1":
+                    num_islands += 1
+                    dfs(row, col)
+                
+        return num_islands
