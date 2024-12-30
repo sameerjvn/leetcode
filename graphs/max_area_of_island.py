8c989969b42155a9e18a734035628e778ef055f5
Sameer Jathavedan <sameerjvn.tech@gmail.com>
2024-12-30 13:57:17 +0100

    graphs: #695 max area of islands (dfs)

--- graphs/max_area_of_island.py
+++ graphs/max_area_of_island.py
@@ -3,6 +3,46 @@
 
 class Solution:
     def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
+        # dfs
+        # time complexity: O(m * n)
+        # space complexity: O(m * n)
+        max_area = 0
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            area = 0
+        
+            # define the base case since dfs is recursive
+            if (
+                r < 0
+                or c < 0
+                or r >= ROWS
+                or c >= COLS
+                or grid[r][c] == 0
+            ):
+                return area
+
+            # do the node modification
+            area += 1
+            grid[r][c] = 0
+
+            # loop through the directions and call dfs 
+            for dr, dc in directions:
+                area += dfs(r + dr, c + dc)
+
+            return area
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 1:
+                    area = dfs(row, col)
+                    max_area = max(max_area, area)
+
+        return max_area
+
+
+    def _maxAreaOfIsland(self, grid: list[list[int]]) -> int:
         # bfs
         # time complexity: O(m * n)
         # space complexity: O(m * n) how?
